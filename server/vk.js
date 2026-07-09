6ff2d13a17733ac690256722f455c433ce327e2e
Renk11 <pavvvlik66@gmail.com>
2026-07-10 01:51:36 +0300

    Refine VK broadcast unsubscribe fallback text

--- server/vk.js
+++ server/vk.js
@@ -125,7 +125,7 @@ export async function sendVkMessageWithOptions(userId, message, options = {}) {
     const { keyboard, ...fallbackOptions } = options;
     const textHints = extractKeyboardTextHints(keyboard);
     const fallbackMessage = textHints.length
-      ? `${message}\n\nЕсли кнопка не отобразилась, ответьте сообщением: ${textHints.join(' / ')}`
+      ? `${message}\n\nДля отписки ответьте: ${textHints.join(' / ')}`
       : message;
 
     return requestVk('messages.send', {
